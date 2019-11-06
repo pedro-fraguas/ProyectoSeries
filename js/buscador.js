2aef2419b525f80e2c084f2f94834b227518ed87
pedro <pfraguas@udesa.edu.ar>
2019-11-06 14:43:01 -0300

    funcion de scroll infinito con boton "ver mas"

--- js/buscador.js
+++ js/buscador.js
@@ -1,25 +1,41 @@
 window.onload = function() {
   var datos = new URLSearchParams(location.search);
   var loBuscado = datos.get("buscador");
+  var series;
+  var imagenes;
 
   fetch("https://api.themoviedb.org/3/search/tv?api_key=935b83cf932d87a1deec2a0108c3513e&language=en-US&query=" + loBuscado + "&page=1")
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    var series = data.results;
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      series = data.results;
+      document.querySelector("h1").innerHTML = "Esto encontramos para tu busqueda de '" + loBuscado + "':"
+      cargarSeries(series);
+    })
+    .catch(function(error) {
+      alert("Error, perdon, vuelva mas tarde")
+    })
 
-    var imagenes = document.querySelectorAll(".serieBuscada a img");
+  function cargarSeries(series) {
+    imagenes = document.querySelectorAll(".serieBuscada a img");
     var hipervinculos = document.querySelectorAll(".serieBuscada a");
-
-    document.querySelector("h1").innerHTML = "Esto encontramos para tu busqueda de '" + loBuscado + "':"
 
     for (var i = 0; i < imagenes.length; i++) {
       imagenes[i].src = "https://image.tmdb.org/t/p/original" + series[i].poster_path;
       hipervinculos[i].href = "DetalleDeSerie.html?idSerie=" + series[i].id;
     }
-  })
-  .catch(function(error) {
-    alert("Error, perdon, vuelva mas tarde")
-  })
+  }
+
+  var verMas = document.querySelector("main button");
+
+  verMas.onclick = function() {
+    var resultados = document.querySelector("div.resultados");
+    resultados.innerHTML += "<article class='serieBuscada'><p></p><a href=''><img src='' alt=''></a></article><article class='serieBuscada'><p></p><a href=''><img src='' alt=''></a></article><article class='serieBuscada'><p></p><a href=''><img src='' alt=''></a></article><article class='serieBuscada'><p></p><a href=''><img src='' alt=''></a></article><article class='serieBuscada'><p></p><a href=''><img src='' alt=''></a></article>";
+    cargarSeries(series);
+    if (imagenes.length >= series.length) {
+      verMas.style.visibility = "hidden";
+    }
+  }
+
 }
