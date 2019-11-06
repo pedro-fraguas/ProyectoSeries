6a770b40b14f5d77d7855507106c81921ae35845
pedro <pfraguas@udesa.edu.ar>
2019-11-06 19:08:14 -0300

    agrego overview a detalle de serie

--- js/detalleDeSerie.js
+++ js/detalleDeSerie.js
@@ -7,11 +7,9 @@ window.onload = function() {
       return response.json();
     })
     .then(function(serie) {
-
       document.querySelector(".poster").src = "https://image.tmdb.org/t/p/original" + serie.poster_path;
-      
       document.querySelector(".tituloPelicula").innerHTML = serie.name;
-
+      document.querySelector(".tituloDatos p").innerHTML = serie.overview;
     })
     .catch(function(error) {
       alert("Error");
