868630fb4acde1d0843cebb1a439cbd3e50e9f7e
pedro <pfraguas@udesa.edu.ar>
2019-11-08 13:52:46 -0300

    copio funciones clave en todos los js

--- js/buscador.js
+++ js/buscador.js
@@ -4,6 +4,23 @@ window.onload = function() {
   var series;
   var imagenes;
 
+  fetch("https://api.themoviedb.org/3/genre/tv/list?api_key=935b83cf932d87a1deec2a0108c3513e&language=en-US")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      var generos = data.genres;
+      var listado = document.querySelectorAll("ul.listado-generos li a");
+
+      for (var i = 0; i < listado.length; i++) {
+        listado[i].innerHTML = generos[i].name;
+        listado[i].href = "SeriesPorGenero.html?idGenero=" + generos[i].id;
+      }
+    })
+    .catch(function(error) {
+      alert("Error");
+    })
+
   fetch("https://api.themoviedb.org/3/search/tv?api_key=935b83cf932d87a1deec2a0108c3513e&language=en-US&query=" + loBuscado + "&page=1")
     .then(function(response) {
       return response.json();
