f3d0ab8bba5fdf9099f49aa57638d934d1b2540e
Federico Sioli <fsioli@udesa.edu.ar>
2019-11-21 19:07:35 -0300

    Agregue codiciones para el buscador avanzado

--- js/buscadorAvanzado.js
+++ js/buscadorAvanzado.js
@@ -13,6 +13,8 @@ window.onload = function(){
     }
   }
 
+
+
   fetch("https://api.themoviedb.org/3/genre/tv/list?api_key=935b83cf932d87a1deec2a0108c3513e&language=en-US")
     .then(function(response) {
       return response.json();
@@ -32,6 +34,17 @@ window.onload = function(){
       alert("Error");
     })
 
+    var avanzado = document.querySelector("form.buscador-avanzado")
+    var ano = document.querySelector("form.buscador-avanzado input.ano")
+    avanzado.onsubmit = function(event){
+
+      if (ano.value.length != 4 || ano.value > 2023){
+        event.preventDefault();
+        alert("Ingrese un año valido")
+      }
+
+    }
+
   // js header
 
   fetch("https://api.themoviedb.org/3/genre/tv/list?api_key=935b83cf932d87a1deec2a0108c3513e&language=en-US")
@@ -46,22 +59,10 @@ window.onload = function(){
         listado[i].innerHTML = generos[i].name;
         listado[i].href = "SeriesPorGenero.html?idGenero=" + generos[i].id;
       }
+
     })
     .catch(function(error) {
       alert("Error");
     })
-
-  // var buscadorAvanzado = document.querySelector("form.buscador-avanzado");
-  // var incluido = document.querySelector("select.generos");
-  // var excluido = document.querySelector("select.generos2");
-  // buscadorAvanzado.onsubmit = function(event) {
-  //   if (incluido == null && excluido == null) {
-  //     event.preventDefault();
-  //     alert("Se debe completar al menos un campo");
-  //   } else if (incluido.value != null && excluido.value != null) {
-  //     event.preventDefault();
-  //     alert("Podes incluir una serie o excluir una, no podes hacer ambas!")
-  //   }
-  // }
 
 }
